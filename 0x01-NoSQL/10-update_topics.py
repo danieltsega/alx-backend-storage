4e9b0afdc8e4c503370d0adf2477755f820a2884
danny <danny@DannyX.myguest.virtualbox.org>
2023-10-17 22:25:45 +0300

    [Feature]: Add a script that updates an attribute

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Update topics
+"""
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Update topics
+    """
+
+    return mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
